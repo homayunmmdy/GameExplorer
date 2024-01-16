2c9db4abe66ba03d3105dcb977626eec3e84d635
Homayoun <blodbyte.c@gmail.com>
2024-01-16 22:12:21 +0330

    Create Card  - Responsive cards  - display platform Icons  - Show critic score

--- src/hooks/useGames.tsx
+++ src/hooks/useGames.tsx
@@ -2,11 +2,20 @@ import { CanceledError } from "axios";
 import { useEffect, useState } from "react";
 import apiClient from "../services/api-client";
 
+export interface Platform { 
+  id: number;
+  name: string;
+  slug: string;
+}
+
 export interface Game {
   id: number;
   name: string;
   background_image: string;
+  parent_platforms: { platform: Platform }[];
+  metacritic: number;
 }
+
 interface FetchGamesResponse {
   count: number;
   results: Game[];
