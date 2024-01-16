c777db45af72b116b96865a357359cf97d653bbc
Homayoun <blodbyte.c@gmail.com>
2024-01-16 20:38:43 +0330

    Responsive layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,27 @@
-import { Button } from '@chakra-ui/react'
-import React from 'react'
+import { Grid, GridItem, Show } from "@chakra-ui/react";
+
 
 const App = () => {
   return (
     <div>
-      <Button colorScheme='blue'>Button</Button>
+       <Grid
+      templateAreas={{
+        base: `"nav" "main"`,
+        lg: `"nav nav" "aside main"`,
+      }}
+    >
+      <GridItem area="nav" bg="coral">
+        Nav
+      </GridItem>
+      <Show above="lg">
+        <GridItem area="aside" bg="gold">
+          Aside
+        </GridItem>
+      </Show>
+      <GridItem area="main" bg="dodgerblue">
+        Main
+      </GridItem>
+    </Grid>
     </div>
   )
 }
